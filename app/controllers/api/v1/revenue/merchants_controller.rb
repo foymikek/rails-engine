ea861842ddbe24e09c39e18effe245dbeacb2010
foymikek <foymikek@gmail.com>
2021-05-27 18:25:40 -0600

    Add passing postman test for merchants revenue.

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -6,4 +6,10 @@ class Api::V1::Revenue::MerchantsController < ApplicationController
       )
     end
   end
+
+  def show
+    merchant = Merchant.find(params[:id])
+
+    render json: MerchantRevenueSerializer.new(merchant)
+  end
 end
